c43f7b4e7b1611e319b28d9dc90307ce78252081
Sam <smscannell@gmail.com>
2022-07-07 12:41:10 +0100

    Completed section 4

---
+++ Section5/BasicIO/main.cpp
@@ -0,0 +1,20 @@
+// Section 5
+// Basic I/O using cin and cout
+
+#include <iostream>
+
+using namespace std;
+
+int main( ){
+
+int num1;
+int num2;
+double num3;
+
+cout << "Enter an integer ";
+cin >> num1;
+cout << "You entered " << num1 <<endl;
+
+
+    
+}
